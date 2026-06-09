417f559993d4b916fcb4c402c0f1008396551589
musango willy <musangowilly@gmail.com>
2026-06-09 18:10:08 +0300

    adding intro.js file

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Musango Willy";
+const age =25;
+const favoriteProgrammingLanguage ="python";
+
+console.log("name:", name);
+console.log("age:", age);
+console.log("favorite programming language:", favoriteProgrammingLanguage);
